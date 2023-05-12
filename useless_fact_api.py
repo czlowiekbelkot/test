b3560693082b70805e5b345d815688c9aba90b3f
czlowiekbelkot <118068962+czlowiekbelkot@users.noreply.github.com>
2023-05-12 08:40:57 +0000

    changed filename

--- useless_fact_api.py
+++ useless_fact_api.py
@@ -7,10 +7,11 @@ print()
 
 
 for i in range(10):
-    response = requests.get("https://uselessfacts.jsph.pl/random.json?language=en")
+    response = requests.get("https://uselessfacts.jsph.pl/random.json?language=en", timeout=5)
     if response.status_code == 200:
         json_data = response.json()
         fuckt = json_data["text"]
         print(f"{i+1}. {fuckt}")
     else:
         print(f"Something wrong with fuckt {i+1}- doesn't work")
+        
