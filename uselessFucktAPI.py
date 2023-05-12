b70df0ea6d245be0555e61160187a8c007a8fab5
czlowiekbelkot <118068962+czlowiekbelkot@users.noreply.github.com>
2023-05-12 08:32:36 +0000

    Writed 10 random useless fact.

---
+++ uselessFucktAPI.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import requests
+
+print("10 seless fuckt:")
+print()
+
+
+for i in range(10):
+    response = requests.get("https://uselessfacts.jsph.pl/random.json?language=en")
+    if response.status_code == 200:
+        json_data = response.json()
+        fuckt = json_data["text"]
+        print(f"{i+1}. {fuckt}")
+    else:
+        print(f"Something wrong with fuckt {i+1}- doesn't work")
