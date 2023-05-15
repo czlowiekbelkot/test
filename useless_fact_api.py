9452fa4b844649b5d0419a7d6b7f6b3c9bd982d0
czlowiekbelkot <118068962+czlowiekbelkot@users.noreply.github.com>
2023-05-15 10:34:44 +0000

    Fixed 'request', added 'id' to response and docstr

--- useless_fact_api.py
+++ useless_fact_api.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
+"""
+Generate 10 random useless facts.
+
+This module generates 10 random useless facts using the uselessfacts.jsph.pl API.
+
+"""
+
 import requests
 
-print("10 seless fuckt:")
+print("10 useless fuckt:")
 print()
 
-
 for i in range(10):
-    response = requests.get("https://uselessfacts.jsph.pl/random.json?language=en", timeout=5)
+    response = requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random?language=en",
+                            timeout=5)
     if response.status_code == 200:
         json_data = response.json()
-        fuckt = json_data["text"]
-        print(f"{i+1}. {fuckt}")
+        fuckt_id = json_data["id"]
+        fuckt_text = json_data["text"]
+        print(f"Id: {fuckt_id} Fact: {fuckt_text}")
     else:
         print(f"Something wrong with fuckt {i+1}- doesn't work")
-        
